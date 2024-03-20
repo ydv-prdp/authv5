bce234729293b51469497430895d93d16b78f4ea
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-03-20 12:29:05 +0530

    user fetch uniquely by id and email

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from "@/schemas";
 import * as  z  from "zod";
+import bcrypt from "bcrypt"
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 
 export const register = async(values: z.infer<typeof RegisterSchema>) => {
@@ -10,6 +13,25 @@ export const register = async(values: z.infer<typeof RegisterSchema>) => {
  if(!validateFields.success){
     return {error: "Invalidate fields!"}
  }
- return {success:"Registered Successfully!"}
+
+ const {email, password, name} = validateFields.data;
+ const hashedPassword = await bcrypt.hash(password, 10);
+
+ const existingUser = await getUserByEmail(email)
+ 
+ if(existingUser){
+   return {error:"Email already in use"}
+ }
+
+ await db.user.create({
+   data:{
+      name,
+      email,
+      password:hashedPassword
+   }
+ })
+
+
+ return {success:"User Created!"}
 }
 
