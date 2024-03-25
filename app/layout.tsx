de3dec93b6560d373da38d6cd090e883477c3224
Pradeep Yadav <pradeepyadav101010@gmail.com>
2024-03-25 10:07:25 +0530

    admin page added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import {SessionProvider} from 'next-auth/react';
 
 import "./globals.css";
 import { auth } from "@/auth";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,10 @@ export default async function RootLayout({
   return (
    <SessionProvider session={session}>   
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Toaster/>
+        {children}
+      </body>
     </html>
    </SessionProvider>
   );
